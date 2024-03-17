87061f6f70c0c2d83c4ebafb023dece25028f852
alanzhang03 <alan.s.zhang@gmail.com>
2024-03-17 01:24:08 -0400

    newest

--- src/App.js
+++ src/App.js
@@ -23,12 +23,6 @@ export default function App() {
           <Route path="/Projects"element={<Projects/>} />
         </Routes>
       </div>   
-      <body>
-        <div>
-        </div>
-        
-      </body>
-      
       <Footer></Footer>
       </>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,7 +21,6 @@ export default function Home() {
           </div>
         </div>
       </body>
-
       <Navbar2></Navbar2>
     </>
   );
