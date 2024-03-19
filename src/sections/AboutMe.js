6bbaf660b7590057993a7e3164003ffe51a635ba
alanzhang03 <alan.s.zhang@gmail.com>
2024-03-18 21:49:53 -0400

    new

--- src/sections/AboutMe.js
+++ src/sections/AboutMe.js
@@ -5,26 +5,39 @@ export default function AboutMe() {
     return (
         <>
         <div>
-            <h1 className='intro-header'>Hi, I'm <span className='purple-text'>Alan</span>, a dedicated full-stack<span className='purple-text'>  web developer!</span></h1>
-            
+            <h1 className='intro-header'>
+
+            Hi, I'm <span className='purple-text'>Alan</span>, 
+            a dedicated full-stack
+            <span className='purple-text'>  web developer!</span>
+
+            </h1>
+
             <p className='about-me-description'>
-            Originally from the outskirts of Philadelphia, Pennsylvania, I am currently immersed in the world
-            of technology and innovation at the University of Maryland, College Park, and am on track to 
-            graduate in May 2025. <br></br><br></br>
+            Originally from the outskirts of <span className='purple-text-description'>Philadelphia, Pennsylvania</span>, 
+            I am currently immersed in the worldof technology and innovation at the 
+            <span className='purple-text-description'> University of Maryland, College Park</span>, and am on track to 
+            graduate in <span className='purple-text-description'> May 2025</span>. 
+            <br></br><br></br>
 
-            As a dedicated developed, I enjoy crafting elegant solutions through code. My academic journey has
+            As a dedicated developed, I enjoy crafting <span className='purple-text-description'> elegant </span> 
+            solutions through code. My <span className='purple-text-description'> academic journey </span> has
             thus far been accompanied with a number of personal projects that not only help to chellenge my 
-            understanding but also amplify my skills in software development.<br></br><br></br>
+            understanding but also <span className='purple-text-description'> amplify my skills </span> 
+            in software development.
+            <br></br><br></br>
 
-            Outside of programming, I enjoy competing on the soccer field and in the virtual realms of video games.
+            Outside of programming, I enjoy competing on the <span className='purple-text-description'>soccer </span>
+            field and in the virtual realms of <span className='purple-text-description'>video games</span>.
             Just as much as I take pleasure in a thrilling match or engaging gameplay session, I equally savor 
-            the journey of learning on platforms like YouTube. <br></br><br></br>
+            the journey of learning on platforms like <span className='purple-text-description'>YouTube</span>. 
+            <br></br><br></br>
 
-            As I naviagte through my education at the University of Maryland, I'm always looking for the next
-            challenge to tackle or problem to solve. Each project is an opportunity to showcase my creativity
-            and innovative mind. Every line of code is a step towards the future in which I am eager to help
-            build.
-
+            As I naviagte through my education at the <span className='purple-text-description'>University of Maryland</span>, 
+            I'm always looking for the next <span className='purple-text-description'>challenge </span>  to tackle or 
+            problem to solve. Each project is an opportunity to showcase my creativity and innovative mind. Every 
+            line of code is a step towards the future in which 
+            I am <span className='purple-text-description'> eager </span> to help build.
             </p> 
 
         </div> 
