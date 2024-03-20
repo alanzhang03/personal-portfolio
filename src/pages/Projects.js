29a784ca5ae996f953a9aef9d9680b1f92341e02
alanzhang03 <alan.s.zhang@gmail.com>
2024-03-19 20:38:31 -0400

    new

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,3 +1,26 @@
 export default function Projects(){
-    return <h1 className="header">Projects</h1>
+    return (
+    <>
+        <h1 className="header">Projects</h1>
+        <body>
+            <div className="projects-container">
+                
+                <div className="project grid">
+
+                </div>
+
+                <div className="project grid">
+
+                </div>
+
+                <div className="project grid">
+
+                </div>
+
+
+            </div>
+        </body>
+    </>
+    );
+
 }
