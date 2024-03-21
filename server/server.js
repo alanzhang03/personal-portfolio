8c9efbc3d62228b5c0d2c4e20dbc40c09b0d0804
alanzhang03 <alan.s.zhang@gmail.com>
2024-03-21 10:43:05 -0400

    new

--- server/server.js
+++ server/server.js
@@ -1,6 +1,3 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
