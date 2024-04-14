6abc3a2da339e3b0aec8f8f53539afbf60b1c1c5
alanzhang03 <alan.s.zhang@gmail.com>
2024-04-14 16:11:32 -0400

    new

--- src/sections/AboutMe.js
+++ src/sections/AboutMe.js
@@ -14,42 +14,19 @@ export default function AboutMe() {
 
             {/*1st paragraph*/}
             <p className='about-me-description'>
-            Originally from the outskirts of 
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://www.google.com/maps/place/Philadelphia,+PA/@40.0026443,-75.2828353,11z/data=!3m1!4b1!4m6!3m5!1s0x89c6b7d8d4b54beb:0x89f514d88c3e58c1!8m2!3d39.9525839!4d-75.1652215!16zL20vMGRjbGc?entry=ttu' 
-            > Philadelphia</a>,
-            I attended 
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://www.usnews.com/education/best-high-schools/pennsylvania/districts/tredyffrin-easttown-school-district/conestoga-high-school-17378' 
-            > Conestoga High School </a> 
-            
-            located in Berwyn, Pennsylvania.
-            I began my software development journey with 
-            
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://www.python.org/' 
-            > Python </a>
-            in a high school course titled 
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://apcentral.collegeboard.org/courses/ap-computer-science-principles/course' 
-            > AP Computer Science Principles</a>.
-            This initial experience ignited my passion for Computer Science, leading me to register for the course
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://apstudents.collegeboard.org/courses/ap-computer-science-a' 
-            > AP Computer Science A </a> 
-            the next year, where I learned 
-             
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://www.java.com/en/' 
-            > Java. </a> 
-            
-            I found both courses immensely enjoyable, thus leading to my 
-            decision to study 
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://www.cs.umd.edu/' 
-            > Computer Science at the University of Maryland, College Park.</a> 
-            <br></br><br></br>
-
+            Originally from the Philadelphia suburbs, I discovered my passion for software development during high school in
+            Berwyn, Pennsylvania,
+            where I learned 
+            Python
+            and
+            Java.
+            Now at the 
+            University of Maryland,
+            I am pursuing a Computer science degree and am particularly interested in the fields of AI, Machine Learning,
+            and web development (using technologies like React and Node.JS). As I approach graduation in May 2025,
+            I am eager to apply my skills to solve real-world problems and continue learning in the tech industry.
+            Feel free to read more about me by clicking the button below, as well as contacting me here to connect or
+            ask questions!
             <Link to="/AboutMePage">
                 <div className='button-container'>
                     <button className='button-to'>
@@ -60,8 +37,11 @@ export default function AboutMe() {
                     </button>
                 </div>
             </Link>
-
             </p>
+
+
+
+
 
             <div className='about-me-footer'>
                 <h1 className='about-me-footer-text'>
