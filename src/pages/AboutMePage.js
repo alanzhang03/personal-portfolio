b71d9c84d4eebb860d0c79441d23ca326c26e64f
alanzhang03 <alan.s.zhang@gmail.com>
2024-06-22 15:39:26 -0400

    editing about me page

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -1,185 +1,375 @@
-import React, { useEffect } from 'react';
-import './pages.css';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import "./pages.css";
+import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa";
-import ScrollToTop from '../sections/ScrollToTop';
+import ScrollToTop from "../sections/ScrollToTop";
+import "../sections/sections.css";
+import JsIcon from "../Assets/js.svg";
+import PythonIcon from "../Assets/python.svg";
+import CIcon from "../Assets/c.svg";
+import HtmlIcon from "../Assets/html.svg";
+import CssIcon from "../Assets/css.svg";
+import OcamlIcon from "../Assets/ocaml.svg";
+import JavaIcon from "../Assets/java.svg";
+import RIcon from "../Assets/r.svg";
+import MatlabIcon from "../Assets/matlab.svg";
+import ReactIcon from "../Assets/react.svg";
+import DjangoIcon from "../Assets/django.svg";
+import GithubIcon from "../Assets/github.svg";
+import OfficeIcon from "../Assets/office.svg";
+import NodejsIcon from "../Assets/nodejs.svg";
+import CSharpIcon from "../Assets/csharp.svg";
+import gitIcon from "../Assets/git.svg";
+import viteIcon from "../Assets/vitejs.svg";
+import vueIcon from "../Assets/vue.svg";
+import webpackIcon from "../Assets/webpack.svg";
 
-export default function AboutMePage() { 
+export default function AboutMePage() {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+	const skills = [
+		{ name: "JavaScript", icon: JsIcon },
+		{ name: "Html", icon: HtmlIcon },
+		{ name: "Css", icon: CssIcon },
+		{ name: "Python", icon: PythonIcon },
+		{ name: "C#", icon: CSharpIcon },
+		{ name: "C", icon: CIcon },
+		{ name: "Ocaml", icon: OcamlIcon },
+		{ name: "Java", icon: JavaIcon },
+		{ name: "R", icon: RIcon },
+		{ name: "Matlab", icon: MatlabIcon },
+	];
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-      }, []);
+	const technologies = [
+		{ name: "React", icon: ReactIcon },
+		{ name: "Webpack", icon: webpackIcon },
+		{ name: "Node", icon: NodejsIcon },
+		{ name: "Vite", icon: viteIcon },
+		{ name: "Vue", icon: vueIcon },
+		{ name: "Django", icon: DjangoIcon },
+		{ name: "Github", icon: GithubIcon },
+		{ name: "git", icon: gitIcon },
+		{ name: "Microsoft Office", icon: OfficeIcon },
+	];
+	return (
+		<>
+			<ScrollToTop></ScrollToTop>
+			<div className="AboutMePage">
+				<h1 className="header">
+					About Me
+					<br></br>
+				</h1>
 
-    return (
-    
-        <>
-            <ScrollToTop></ScrollToTop>
-            <div className='AboutMePage'>
-
-            <h1 className='header'>
-                About Me
-                <br></br>
-            </h1>
-            
-            {/*1st paragraph*/}
-            <p className='about-me-description'>
-            
-            Originally from the outskirts of 
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://www.google.com/maps/place/Philadelphia,+PA/@40.0026443,-75.2828353,11z/data=!3m1!4b1!4m6!3m5!1s0x89c6b7d8d4b54beb:0x89f514d88c3e58c1!8m2!3d39.9525839!4d-75.1652215!16zL20vMGRjbGc?entry=ttu' 
-            > Philadelphia</a>,
-            I attended 
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://www.usnews.com/education/best-high-schools/pennsylvania/districts/tredyffrin-easttown-school-district/conestoga-high-school-17378' 
-            > Conestoga High School </a> 
-            
-            located in Berwyn, Pennsylvania.
-            I began my software development journey with 
-            
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://www.python.org/' 
-            > Python </a>
-            in a high school course titled 
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://apcentral.collegeboard.org/courses/ap-computer-science-principles/course' 
-            > AP Computer Science Principles</a>.
-            This initial experience ignited my passion for Computer Science, leading me to register for the course
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://apstudents.collegeboard.org/courses/ap-computer-science-a' 
-            > AP Computer Science A </a> 
-            the next year, where I learned 
-             
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://www.java.com/en/' 
-            > Java. </a> 
-            
-            I found both courses immensely enjoyable, thus leading to my 
-            decision to study 
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://www.cs.umd.edu/' 
-            > Computer Science at the University of Maryland, College Park.</a> 
-            <br></br><br></br>
-
-            {/*2nd paragraph*/}
-            Currently in College, I am on track to graduate in May 2025. I have taken classes which have covered
-            fundamental conecepts such as 
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://planetterp.com/course/CMSC351' 
-            > Algorithms, </a> 
-             
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://planetterp.com/course/CMSC132' 
-            > OOP, </a> 
-             
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://planetterp.com/course/CMSC250' 
-            > Discrete Structures, </a>
-
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://planetterp.com/course/CMSC216' 
-            > Operating Systems,  </a> 
-            
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://planetterp.com/course/CMSC216' 
-            > Memory Management </a> and
-          
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://planetterp.com/course/CMSC351' 
-            > Time Complexity Analysis </a> among others. Looking ahead, I am excited to expand my skillset in 
-             
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://planetterp.com/course/CMSC421' 
-            > AI, </a>
-               
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://planetterp.com/course/CMSC422' 
-            > Machine Learning, </a>
-               
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://planetterp.com/course/CMSC430' 
-            > Compilers, </a> and programming with 
-
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://planetterp.com/course/CMSC436' 
-            > Handheld Systems </a> in the near future.
-            
-            <br></br><br></br>
-
-
-            <center>
-                <h1 className='header'>
-                Web Development
-                <br></br>
-                </h1>
-            </center>
-
-            {/*3rd paragraph*/}
-            Throughout my software development journey thus far, I have been particularly interested in 
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://www.geeksforgeeks.org/web-development/' 
-            > web development</a>,
-            
-            a field I first explored in the summer of 2023. I was especially interested in what the process was 
-            like, creating an 
-
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://www.geeksforgeeks.org/web-development/#front_dev' 
-            > eye catching and responsive </a> page as well as some of the core features behind how these websites were 
-    
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://www.geeksforgeeks.org/web-development/#back_dev' 
-            > deployed and running in the background. </a> This led to my interest in learning about technologies like
-
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://react.dev/' 
-            > React </a> and
-
-            <a target="_blank" rel='noreferrer' className='purple-text-description'
-            href='https://nodejs.org/en' 
-            > Node.js, </a> which I decided to use to build this
-
-            <Link to="/Home" className='purple-text-description'> web portfolio </Link>
-
-            <br></br><br></br>
-
-            {/*4th paragraph*/}
-            As I approach the end of my academic journey, I am excited and hopeful to apply the 
-            <Link to="/Home" className='purple-text-description'> skills and knowledge </Link>
-
-            I have gained thus far to solve real-world problems. I am particularly excited about the opportunitites 
-            to take on new challenges, meet new people, and use new technologies. I am always seeking to contribute
-            to meaningful 
-
-            <Link to="/Projects" className='purple-text-description'> projects </Link>
-            and expand upon my current skill set through continuous learning and collaboration,
-            and am excited for whats to come.
-            <br></br><br></br>
-            
-            {/*5th paragraph*/}
-            If you have any further questions or even if you would just like to say hello, feel free to navigate to the 
-            <Link to="/Contact" className='purple-text-description'> contact page </Link>
-            of this
-            <Link to="/Home" className='purple-text-description'> portfolio </Link>
-            and shoot me a message! 
-            <br></br><br></br>
-
-            <Link to="/Contact">
-                <div className='button-container'>
-                    <button className='button-to'>
-                        <div className='button-text'>
-                            Contact me!
-                            <FaArrowRight className='right-arrow'></FaArrowRight>                 
-                        </div>
-                    </button>
-                </div>
-            </Link>
-
-            </p>
-
-            </div>     
-        </>
-    )
+				{/*1st paragraph*/}
+				<p className="about-me-description">
+					Originally from the outskirts of
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://www.google.com/maps/place/Philadelphia,+PA/@40.0026443,-75.2828353,11z/data=!3m1!4b1!4m6!3m5!1s0x89c6b7d8d4b54beb:0x89f514d88c3e58c1!8m2!3d39.9525839!4d-75.1652215!16zL20vMGRjbGc?entry=ttu"
+					>
+						{" "}
+						Philadelphia
+					</a>
+					, I attended
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://www.usnews.com/education/best-high-schools/pennsylvania/districts/tredyffrin-easttown-school-district/conestoga-high-school-17378"
+					>
+						{" "}
+						Conestoga High School{" "}
+					</a>
+					located in Berwyn, Pennsylvania. I began my software development
+					journey with
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://www.python.org/"
+					>
+						{" "}
+						Python{" "}
+					</a>
+					in a high school course titled
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://apcentral.collegeboard.org/courses/ap-computer-science-principles/course"
+					>
+						{" "}
+						AP Computer Science Principles
+					</a>
+					. This initial experience ignited my passion for Computer Science,
+					leading me to register for the course
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://apstudents.collegeboard.org/courses/ap-computer-science-a"
+					>
+						{" "}
+						AP Computer Science A{" "}
+					</a>
+					the next year, where I learned
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://www.java.com/en/"
+					>
+						{" "}
+						Java.{" "}
+					</a>
+					I found both courses immensely enjoyable, thus leading to my decision
+					to study
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://www.cs.umd.edu/"
+					>
+						{" "}
+						Computer Science at the University of Maryland, College Park.
+					</a>
+					<br></br>
+					<br></br>
+					{/*2nd paragraph*/}
+					Currently in College, I am on track to graduate in May 2025. I have
+					taken classes which have covered fundamental conecepts such as
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://planetterp.com/course/CMSC351"
+					>
+						{" "}
+						Algorithms,{" "}
+					</a>
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://planetterp.com/course/CMSC132"
+					>
+						{" "}
+						OOP,{" "}
+					</a>
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://planetterp.com/course/CMSC250"
+					>
+						{" "}
+						Discrete Structures,{" "}
+					</a>
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://planetterp.com/course/CMSC216"
+					>
+						{" "}
+						Operating Systems,{" "}
+					</a>
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://planetterp.com/course/CMSC216"
+					>
+						{" "}
+						Memory Management{" "}
+					</a>{" "}
+					and
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://planetterp.com/course/CMSC351"
+					>
+						{" "}
+						Time Complexity Analysis{" "}
+					</a>{" "}
+					among others. Looking ahead, I am excited to expand my skillset in
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://planetterp.com/course/CMSC421"
+					>
+						{" "}
+						AI,{" "}
+					</a>
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://planetterp.com/course/CMSC422"
+					>
+						{" "}
+						Machine Learning,{" "}
+					</a>
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://planetterp.com/course/CMSC430"
+					>
+						{" "}
+						Compilers,{" "}
+					</a>{" "}
+					and programming with
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://planetterp.com/course/CMSC436"
+					>
+						{" "}
+						Handheld Systems{" "}
+					</a>{" "}
+					in the near future.
+					<br></br>
+					<br></br>
+					<center>
+						<h1 className="header">
+							Web Development
+							<br></br>
+						</h1>
+					</center>
+					{/*3rd paragraph*/}
+					Throughout my software development journey thus far, I have been
+					particularly interested in
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://www.geeksforgeeks.org/web-development/"
+					>
+						{" "}
+						web development
+					</a>
+					, a field I first explored in the summer of 2023. I was especially
+					interested in what the process was like, creating an
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://www.geeksforgeeks.org/web-development/#front_dev"
+					>
+						{" "}
+						eye catching and responsive{" "}
+					</a>{" "}
+					page as well as some of the core features behind how these websites
+					were
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://www.geeksforgeeks.org/web-development/#back_dev"
+					>
+						{" "}
+						deployed and running in the background.{" "}
+					</a>{" "}
+					This led to my interest in learning about technologies like
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://react.dev/"
+					>
+						{" "}
+						React{" "}
+					</a>{" "}
+					and
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="purple-text-description"
+						href="https://nodejs.org/en"
+					>
+						{" "}
+						Node.js,{" "}
+					</a>{" "}
+					which I decided to use to build this
+					<Link to="/Home" className="purple-text-description">
+						{" "}
+						web portfolio{" "}
+					</Link>
+					<br></br>
+					<br></br>
+					{/*4th paragraph*/}
+					As I approach the end of my academic journey, I am excited and hopeful
+					to apply the
+					<Link to="/Home" className="purple-text-description">
+						{" "}
+						skills and knowledge{" "}
+					</Link>
+					I have gained thus far to solve real-world problems. I am particularly
+					excited about the opportunitites to take on new challenges, meet new
+					people, and use new technologies. I am always seeking to contribute to
+					meaningful
+					<Link to="/Projects" className="purple-text-description">
+						{" "}
+						projects{" "}
+					</Link>
+					and expand upon my current skill set through continuous learning and
+					collaboration, and am excited for whats to come.
+					<br></br>
+					<br></br>
+					{/*5th paragraph*/}
+					If you have any further questions or even if you would just like to
+					say hello, feel free to navigate to the
+					<Link to="/Contact" className="purple-text-description">
+						{" "}
+						contact page{" "}
+					</Link>
+					of this
+					<Link to="/Home" className="purple-text-description">
+						{" "}
+						portfolio{" "}
+					</Link>
+					and shoot me a message!
+					<br></br>
+					<br></br>
+					<Link to="/Contact">
+						<div className="button-container">
+							<button className="button-to">
+								<div className="button-text">
+									Contact me!
+									<FaArrowRight className="right-arrow"></FaArrowRight>
+								</div>
+							</button>
+						</div>
+					</Link>
+				</p>
+			</div>
+			<div className="Skills">
+				<h1 className="header">Programming Languages</h1>
+				<div className="skills-grid">
+					{skills.map((skill) => (
+						<div className="skill-item" key={skill.name}>
+							<img src={skill.icon} alt={skill.name} />
+							<p className="skill-name">{skill.name}</p>
+						</div>
+					))}
+				</div>
+				<h1 className="header">Technologies</h1>
+				<div className="technologies-grid">
+					{technologies.map((tech) => (
+						<div className="technology-item" key={tech.name}>
+							<img src={tech.icon} alt={tech.name} />
+							<p className="tech-name">{tech.name}</p>
+						</div>
+					))}
+				</div>
+			</div>
+		</>
+	);
 }
-
-
-
