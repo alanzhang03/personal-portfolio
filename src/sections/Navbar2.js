6efb71111dda44fd16682cbf92286a9a94c43eb3
alanzhang03 <alan.s.zhang@gmail.com>
2024-06-23 01:48:00 -0400

    padding

--- src/sections/Navbar2.js
+++ src/sections/Navbar2.js
@@ -1,47 +1,44 @@
 import { useRef } from "react";
-import './sections.css';
+import "./sections.css";
 import ScrollToTop from "./ScrollToTop";
-import AboutMe from "./AboutMe"; 
 import Contact from "./Contact";
 import Skills from "./Skills";
-import StarField from "../pages/StarField";
-
 
 export default function Navbar2() {
-    const aboutMeRef = useRef(null); 
-    const contactRef = useRef(null);
-    const skillsRef = useRef(null);
-    
+	const technologiesRef = useRef(null);
+	const skillsRef = useRef(null);
 
-    const scrollToSection = (elementRef) => {
-        window.scrollTo({
-            top: elementRef.current.offsetTop,
-            behavior: 'smooth'
-        });
-    };
+	const scrollToSection = (elementRef) => {
+		window.scrollTo({
+			top: elementRef.current.offsetTop,
+			behavior: "smooth",
+		});
+	};
 
-    return (
-        <>
-            <ScrollToTop />
-            <div className="Nav2">
-                <ul2>
-                    <li2 onClick={() => scrollToSection(skillsRef)} className="link">
-                        <i class="fa-solid fa-circle"></i>
-                    </li2>
-                    <li2 onClick={() => scrollToSection(contactRef)} className="link">
-                        <i class="fa-solid fa-circle"></i>
-                    </li2>
-                </ul2>
-            </div>
+	return (
+		<>
+			<ScrollToTop />
+			<div className="Nav2">
+				<ul2>
+					<li2 onClick={() => scrollToSection(skillsRef)} className="link">
+						<i class="fa-solid fa-circle"></i>
+					</li2>
+					<li2
+						onClick={() => scrollToSection(technologiesRef)}
+						className="link"
+					>
+						<i class="fa-solid fa-circle"></i>
+					</li2>
+				</ul2>
+			</div>
 
+			<div ref={skillsRef} className="Skills">
+				<Skills />
+			</div>
 
-            <div ref={skillsRef} className="Skills">
-                <Skills />
-            </div>
-
-            <div ref={contactRef} className="Contact">
-                <Contact />
-            </div>
-        </>
-    );  
+			<div ref={technologiesRef} className="Contact">
+				<Contact />
+			</div>
+		</>
+	);
 }
